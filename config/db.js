1d035f7ba41cd646c9eb7dedb65bae349d96ed85
Junitrollnita <Junitrollnita@users.noreply.github.com>
2020-01-03 03:54:58 -0300

    Tentando de novo

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports ={mongoURI: "mongodb+srv://junitroll:94219489@blogapp-2ypwg.mongodb.net/test?retryWrites=true&w=majority"}
+    module.exports ={mongoURI: "mongo_blogapp-prod:27017"}
 }else{
     module.exports ={mongoURI: 'mongodb://localhost/blogapp'}
 }

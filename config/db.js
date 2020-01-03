bd5243101aee405a3923b8f9462414cb7d8b86eb
Junitrollnita <Junitrollnita@users.noreply.github.com>
2020-01-03 03:45:51 -0300

    Tentando de novo

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports ={mongoURI: "mongodb://junitroll:94219489@blogapp-shard-00-00-2ypwg.mongodb.net:27017,blogapp-shard-00-01-2ypwg.mongodb.net:27017,blogapp-shard-00-02-2ypwg.mongodb.net:27017/test?ssl=true&replicaSet=blogapp-shard-0&authSource=admin&retryWrites=true&w=majority"}
+    module.exports ={mongoURI: "mongodb+srv://junitroll:<password>@blogapp-2ypwg.mongodb.net/test?retryWrites=true&w=majority"}
 }else{
     module.exports ={mongoURI: 'mongodb://localhost/blogapp'}
 }
